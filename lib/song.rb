8763194c1b7b4f0bd1d48ada5ebc0ebc6ee56509
Dakota Wicker <dwicker1996@gmail.com>
2020-04-01 14:24:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,30 +18,18 @@ class Song
     @@count
   end
 
-  def self.genres
-    @@genres.uniq!
-  end
-
   def self.artists
     @@artists.uniq!
   end
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre] #if the genre is already in the hash
-        genre_count[genre] += 1
-      else #if it's a new genre
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
+  def self.genres
+    @@genres.uniq!
   end
 
   def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
+      if artist_count[artist] 
         artist_count[artist] += 1
       else
         artist_count[artist] = 1
@@ -49,4 +37,17 @@ class Song
     end
     artist_count
   end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] 
+        genre_count[genre] += 1
+      else 
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
 end
