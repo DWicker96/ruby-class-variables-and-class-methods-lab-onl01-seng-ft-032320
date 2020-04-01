59982550352aa21be5c02fafc6894ced2ee8f59b
Dakota Wicker <dwicker1996@gmail.com>
2020-04-01 14:03:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,18 @@ attr_accessor :count, :artist, :genre
  @@count = 0
  @@artist = []
  @@genre = []
+ def initialize(song_name, artist, genre)
+   @song = song_name
+   @artist = artist
+   @genre = genre 
+   @@count += 1
+   @@artist << artist 
+   @@genre << genre
+end
+   
   def count
+    
+    
 
   end
 
